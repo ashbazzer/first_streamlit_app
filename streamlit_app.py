f38b342637479dba7843476b27ec4c5d2b6e5b15
ashbazzer <39597063+ashbazzer@users.noreply.github.com>
2023-03-17 09:56:42 +0000

    Update streamlit_app.py
    lookup fruit choice and display in table

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,13 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Banana')
+streamlit.write('The user entered ', fruit_choice)
+
+# lookup fruit choice
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+
+# normalise json resonse
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# creates dataframe and displays as table
+streamlit.dataframe(fruityvice_normalized)
